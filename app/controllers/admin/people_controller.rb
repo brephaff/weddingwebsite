ccae486534f5b4870b831c8d4570f8cfd854158a
Bre Phaff <brephaff@gmail.com>
2016-03-28 22:27:42 -0700

    Added ability to edit people on index page
    Closes issue #17

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -18,6 +18,25 @@ class Admin::PeopleController < Admin::ApplicationController
     render(:new)
   end
 
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    @person.update_attributes!(person_params)
+    redirect_to(admin_people_path)
+
+  rescue ActiveRecord::RecordInvalid
+    render(:edit)
+  end
+
+  def destroy
+    @person = Person.find(params[:id])
+    @person.destroy
+    redirect_to(admin_people_path)
+  end
+
   def upload
     PersonImport.from_xlsx(params[:file].path, :sheet_name => 'Head Count')
     redirect_to(admin_people_path)
@@ -26,6 +45,6 @@ class Admin::PeopleController < Admin::ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:first_name)
+    params.require(:person).permit(:first_name, :last_name, :email_address, :title, :household_id)
   end
 end
