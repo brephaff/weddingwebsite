31f49673c73f706a1c43e986d6c8a537c872d34a
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-08-20 23:16:46 -0700

    Add parallax scrolling to photo_section background

---
+++ app/assets/javascripts/application/parallax_scrolling.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('[data-behaviour~="parallax-bg"]').each(function(){
+      var $bgobj = $(this); // assigning the object
+      var speed = $bgobj.data('speed')
+      $window = $(window)
+      $window.scroll(function() {
+          var yPos = -($window.scrollTop() / speed);
+
+          // Put together our final background position
+          var coords = '50% '+ yPos + 'px';
+
+          // Move the background
+          $bgobj.css({ backgroundPosition: coords });
+      });
+  });
+});
