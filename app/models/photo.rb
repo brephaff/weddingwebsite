7cc74c04d92f0da11be048e925daf160a00b9099
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-08-21 11:22:22 -0700

    Separate pending and accepted photos in admin interface

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ActiveRecord::Base
   mount_uploader :file, FileUploader
 
+  scope :pending, lambda { where(:accepted => false) }
   scope :accepted, lambda { where(:accepted => true) }
 end
