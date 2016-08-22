089905f66764868e6bd1fe0607e4a3ac681cb802
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-08-22 00:51:38 -0700

    Remove aws credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -15,11 +15,7 @@ CarrierWave.configure do |config|
 end
 
 S3DirectUpload.config do |c|
-  # c.access_key_id = Rails.application.secrets.s3_key # your access key id
-  # c.secret_access_key = Rails.application.secrets.s3_secret # your secret access key
-  # c.bucket = Rails.application.secrets.s3_bucket_name # your bucket name
-
-c.access_key_id = 'AKIAIXCJJT7HZ5BYQCXA'
-c.secret_access_key = 'e1mUe7IGIiH9F3EXj17dak8/zX7sDAm8yna1Hxy1'
-c.bucket = 'nickandbrewedding'
+  c.access_key_id = Rails.application.secrets.s3_key # your access key id
+  c.secret_access_key = Rails.application.secrets.s3_secret # your secret access key
+  c.bucket = Rails.application.secrets.s3_bucket_name # your bucket name
 end
