47a7be993b6663c9d9ac8866dc42a351dada54d8
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-08-22 20:57:40 -0700

    Revert to fixed+cover parallax scrolling, disable on mobile

---
+++ app/assets/javascripts/application/detect_mobile.js
@@ -0,0 +1,4 @@
+if (/mobile/i.test(navigator.userAgent)) {
+  $('html').addClass('mobile')
+  window.isMobile = true
+}
--- app/assets/javascripts/application/parallax_scrolling.js
+++ app/assets/javascripts/application/parallax_scrolling.js
@@ -1,7 +1,10 @@
-$(function(){
+//= require ./detect_mobile
+
+if (!window.isMobile){
   $('[data-behaviour~="parallax-bg"]').each(function(){
       var $bgobj = $(this); // assigning the object
       var speed = $bgobj.data('speed')
+      var offset = $bgobj.data('offset') || 0
       $window = $(window)
       $window.scroll(update)
       update();
@@ -10,10 +13,10 @@ $(function(){
         var yPos = -($window.scrollTop() / speed);
 
         // Put together our final background position
-        var coords = 'center bottom '+ yPos + 'px';
+        var coords = '50% '+ yPos + 'px';
 
         // Move the background
         $bgobj.css({ backgroundPosition: coords });
       }
   });
-});
+}
