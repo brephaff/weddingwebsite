004898852e4d429a9173634d0e3b427912a814fb
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-08-25 12:43:47 -0700

    Redirect after notifying server of all uploads
    Before we would just wait until they had been uploaded to S3, but would redirect to the success page without waiting to notify the application server.

--- app/assets/javascripts/application/forms.js
+++ app/assets/javascripts/application/forms.js
@@ -7,19 +7,35 @@ $('[data-behaviour=on-value-show]').change(function(){
   } else {
     $(target).hide()
   }
-}).change()
+}).change();
 
-$('#photos_upload_form')
-  .S3Uploader({
-    path: 'originals/',
-    progress_bar_target: $('#photos_upload_form .upload_progress')
-  })
-  .on('s3_uploads_start', function(){
-    $(this).find('[data-disable-with]').each(function(){
-      $(this).text($(this).data('disable-with'))
+
+(function($){
+  var notifyPending = []
+  var allStored = false
+
+  $('#photos_upload_form')
+    .S3Uploader({
+      path: 'originals/',
+      progress_bar_target: $('#photos_upload_form .upload_progress')
     })
-    $(this).css('pointerEvents', 'none')
-  })
-  .on('s3_uploads_complete', function(){
-    window.location = '/photos/uploaded'
-  })
+    .on('s3_uploads_start', function(){
+      $(this).find('[data-disable-with]').each(function(){
+        $(this).text($(this).data('disable-with'))
+      })
+      $(this).css('pointerEvents', 'none')
+    })
+    .on('s3_upload_complete', function(){
+      notifyPending.push(true)
+    })
+    .on('s3_uploads_complete', function(){
+      allStored = true
+    })
+    .on('ajax:complete', function(){
+      notifyPending.pop()
+      if (allStored && !notifyPending.length) {
+        window.location = '/photos/uploaded'
+      }
+    })
+
+})($)
