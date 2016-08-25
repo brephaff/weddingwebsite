2617b296701707ff1b4f2216d4d83be997ec7609
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-08-25 12:56:38 -0700

    Add button to accept all photos

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -6,9 +6,18 @@ class Admin::PhotosController < Admin::ApplicationController
   end
 
   def accept
-    @photo = Photo.find(params[:id])
-    @photo.update_attributes(:accepted => true, :attachment => open(@photo.original_url))
-    @photo.attachment.recreate_versions!
+    accept_photo(Photo.find(params[:id]))
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render :nothing => true }
+    end
+  end
+
+  def accept_all
+    Photo.pending.find_each do |photo|
+      accept_photo(photo)
+    end
 
     respond_to do |format|
       format.html { redirect_to :back }
@@ -24,5 +33,12 @@ class Admin::PhotosController < Admin::ApplicationController
       format.html { redirect_to :back }
       format.js { render :nothing => true }
     end
+  end
+
+  private
+
+  def accept_photo(photo)
+    photo.update_attributes(:accepted => true, :attachment => open(photo.original_url))
+    photo.attachment.recreate_versions!
   end
 end
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -4,6 +4,11 @@ module PhotosHelper
     link_to 'Accept', [:accept, :admin, photo], html_options
   end
 
+  def link_to_accept_all_photos(html_options = {})
+    html_options = html_options.reverse_merge :remote => true, :method => :post
+    link_to 'Accept All', [:accept_all, :admin, Photo], html_options
+  end
+
   def link_to_delete_photo(photo, html_options = {})
     html_options = html_options.reverse_merge :method => :delete, :remote => true, :data => {:confirm => 'Are you sure you want to delete this photo?'}
     link_to 'Delete', [:admin, photo], html_options
