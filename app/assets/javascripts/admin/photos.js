2745f56f054e8cec85d05b2e1393805f44ab33f3
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-08-26 11:17:45 -0700

    Sequentially accept all
    This avoids overwhelming the server all at once.

--- app/assets/javascripts/admin/photos.js
+++ app/assets/javascripts/admin/photos.js
@@ -4,3 +4,14 @@ $(document).on('ajax:send', '[data-behaviour~=accept-photo]', function(){
 $(document).on('ajax:send', '[data-behaviour~=delete-photo]', function(){
   $(this).closest('tr').remove()
 })
+$(document).on('click', '[data-behaviour~=accept-all-photos]', function(){
+  var links = $.makeArray(('[data-behaviour~=accept-photo]'))
+
+  function triggerNext() {
+    current = $(links.shift())
+    current.one('ajax:complete', triggerNext)
+    current.click()
+  }
+
+  triggerNext()
+})
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,15 +1,16 @@
 module PhotosHelper
   def link_to_accept_photo(photo, html_options = {})
-    html_options = html_options.reverse_merge :remote => true, :method => :post
+    html_options = html_options.reverse_merge :remote => true, :method => :post, 'data-behaviour' => 'accept-photo'
     link_to 'Accept', [:accept, :admin, photo], html_options
   end
 
   def link_to_accept_all_photos(html_options = {})
-    link_to 'Accept All', "javascript:$('[data-behaviour~=accept-photo]').click()", html_options
+    content_tag :span, 'Accept All', html_options.merge('data-behaviour' => 'accept-all-photos')
   end
 
   def link_to_delete_photo(photo, html_options = {})
     html_options = html_options.reverse_merge :method => :delete, :remote => true, :data => {:confirm => 'Are you sure you want to delete this photo?'}
+    html_options.merge! 'data-behaviour' => 'delete-photo'
     link_to 'Delete', [:admin, photo], html_options
   end
 end
